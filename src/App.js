02dd4acbc36da80e2a6183055790fdc8904d075f
Vaida Aurel <Vaida_Aurel@yahoo.com>
2023-07-17 13:37:21 +0300

    create a TaskCard and add CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import MyFirstComponent from "./components/MyFirstComponent";
+import TaskCard from "./components/task-card/TaskCard";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <MyFirstComponent />
+        <TaskCard />
       </header>
     </div>
   );
---
+++ src/components/task-card/TaskCard.js
@@ -0,0 +1,7 @@
+import "../task-card/TaskCard.css";
+
+function TaskCard() {
+  return <div className="card-wapper">Some Text</div>;
+}
+
+export default TaskCard;
