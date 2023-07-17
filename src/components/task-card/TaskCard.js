87fb537cb5c6eeea2f07e903ac63da0bb72a90b7
Vaida Aurel <Vaida_Aurel@yahoo.com>
2023-07-17 14:07:55 +0300

    add font(html) ,css, and div for TaskCard

--- src/components/task-card/TaskCard.js
+++ src/components/task-card/TaskCard.js
@@ -1,7 +1,25 @@
 import "../task-card/TaskCard.css";
 
 function TaskCard() {
-  return <div className="card-wapper">Some Text</div>;
+  return (
+    <div className="card-wapper">
+      <div className="card-header">
+        <p className="task-id">T-1</p>
+        <div className="badge">
+          <p>ToDo</p>
+        </div>
+      </div>
+      <div className="card-content">
+        <p>Create a Design System for Enum Workspace.</p>
+      </div>
+      <div className="card-footer">
+        <div>
+          <p>Due Date</p>
+          <p>17/07/2023</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default TaskCard;
