e04346092e9ae2939579e4343dfa4978334b8943
Vaida Aurel <Vaida_Aurel@yahoo.com>
2023-07-21 22:35:47 +0300

    Evenimente Sintetice

--- src/components/forms/CreateTaskForm.jsx
+++ src/components/forms/CreateTaskForm.jsx
@@ -1,24 +1,52 @@
 import React from "react";
 import "./CreateTaskForm.css";
 const CreateTaskForm = () => {
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  const handleDateChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  const handleDetailsChange = (event) => {
+    console.log(event.target.value);
+  };
   return (
     <div>
       <form>
         <div className="form-row">
-          <label>Task Name</label>
-          <input type="text" />
+          <label className="label-md">Task Name</label>
+          <input
+            onChange={handleNameChange}
+            className="input-primary"
+            type="text"
+          />
         </div>
 
         <div className="form-row">
-          <label>Due Date</label>
-          <input type="date" />
+          <label className="label-md">Due Date</label>
+          <input
+            onChange={handleDateChange}
+            className="input-primary"
+            type="date"
+          />
         </div>
         <div className="form-row">
-          <label>Task Details</label>
-          <textarea name="" id="" cols="30" rows="10"></textarea>
+          <label className="label-md">Task Details</label>
+          <textarea
+            onChange={handleDetailsChange}
+            className="input-primary"
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+          ></textarea>
         </div>
 
-        <button type="submit">Create Task</button>
+        <button className="button-primary" type="submit">
+          Create Task
+        </button>
       </form>
     </div>
   );
