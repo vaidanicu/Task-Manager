db36318a9d54bc77e5b40ed1472e3ed3935ac62a
Vaida Aurel <Vaida_Aurel@yahoo.com>
2023-07-21 12:36:45 +0300

    Evenimente in React

--- src/components/task-card/TaskCard.js
+++ src/components/task-card/TaskCard.js
@@ -3,10 +3,17 @@ import "../task-card/TaskCard.css";
 import DateContainer from "../date-container/DateContainer";
 
 function TaskCard(props) {
+  let taskId = props.id;
+
+  const handleClick = () => {
+    console.log("Click" + props.Id);
+    taskId = taskId + "Clicked!";
+  };
+
   return (
     <div className="card-wapper">
       <div className="card-header">
-        <p className="task-id">{props.Id}</p>
+        <p className="task-id">{taskId}</p>
         <Badge status={props.status} />
       </div>
 
@@ -14,6 +21,7 @@ function TaskCard(props) {
         <p>{props.Name}</p>
       </div>
       <div className="card-footer">
+        <button onClick={handleClick}>ClickMe</button>
         <DateContainer date={props.dueDate} />
       </div>
     </div>
