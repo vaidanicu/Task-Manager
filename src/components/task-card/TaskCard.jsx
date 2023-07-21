65ce53343f1df3045028c64547dada407b43bf01
Vaida Aurel <Vaida_Aurel@yahoo.com>
2023-07-21 13:36:32 +0300

    add counter fn useState

--- src/components/task-card/TaskCard.jsx
+++ src/components/task-card/TaskCard.jsx
@@ -4,26 +4,27 @@ import "../task-card/TaskCard.css";
 import DateContainer from "../date-container/DateContainer";
 
 function TaskCard(props) {
-  const [taskId, setTaskId] = useState(props.Id);
+  const [counter, setCounter] = useState(0);
+  const [taskId, setTaskId] = useState();
 
   const handleClick = () => {
-    setTaskId("Clicked!");
-    console.log(taskId);
+    setCounter(counter + 1);
   };
 
   return (
     <div className="card-wapper">
       <div className="card-header">
-        <p className="task-id">{taskId}</p>
+        <p className="task-id">{props.Id}</p>
         <Badge status={props.status} />
       </div>
 
       <div className="card-content">
-        <p>{props.Name}</p>
+        {/* <p>{props.Name}</p> */}
+        <p>{counter}</p>
       </div>
       <div className="card-footer">
-        <button onClick={handleClick}>ClickMe</button>
-        <DateContainer date={props.dueDate} />
+        <button onClick={handleClick}>INCREMENT</button>
+        {/* <DateContainer date={props.dueDate} /> */}
       </div>
     </div>
   );
