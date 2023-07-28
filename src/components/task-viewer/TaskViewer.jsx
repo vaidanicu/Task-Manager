f5215ffb8da6eebe69f55b9878b48ea4620a74cb
Vaida Aurel <Vaida_Aurel@yahoo.com>
2023-07-28 06:39:04 +0300

    add EmplyListMessage

--- src/components/task-viewer/TaskViewer.jsx
+++ src/components/task-viewer/TaskViewer.jsx
@@ -1,25 +1,31 @@
 import TaskCard from "../task-card/TaskCard";
 import "../task-viewer/TaskViewer.css";
 import ControlPanel from "../control-panel/ControlPanel";
+import EmptyListMessage from "../empty-list-message/EmptyListMessage";
 
 function TaskViewer(props) {
-  const todoItems = props.taskList.filter((item) => item.status === "ToDo");
-
   return (
     <div className="task-viewer-container">
       <ControlPanel OnNewTaskAdd={props.OnNewTaskAdd} />
+
       <div className="task-list-container">
-        {todoItems.map((item) => {
-          return (
-            <TaskCard
-              key={item.id}
-              Id={item.id}
-              status={item.status}
-              Name={item.Name}
-              dueDate={item.dueDate}
-            />
-          );
-        })}
+        {props.taskList.length > 0 ? (
+          <div className="task-list-grid">
+            {props.taskList.map((item) => {
+              return (
+                <TaskCard
+                  key={item.id}
+                  Id={item.id}
+                  status={item.status}
+                  Name={item.Name}
+                  dueDate={item.dueDate}
+                />
+              );
+            })}
+          </div>
+        ) : (
+          <EmptyListMessage />
+        )}
       </div>
     </div>
   );
